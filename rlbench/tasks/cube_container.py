eefedb12d757ffe9b5e6b364fd06c80b03ae22b7
SimonReese <59612841+SimonReese@users.noreply.github.com>
2026-04-08 13:32:07 +0200

    Adds 2 more variations for robot perspective

--- rlbench/tasks/cube_container.py
+++ rlbench/tasks/cube_container.py
@@ -4,6 +4,7 @@ from typing import List, Tuple
 import numpy
 from pyrep.objects.object import Object
 from rlbench.backend.conditions import DetectedCondition
+from rlbench.backend.spawn_boundary import SpawnBoundary
 from rlbench.backend.task import Task
 
 from pyrep.objects import Dummy, ProximitySensor, Shape
@@ -29,6 +30,9 @@ class CubeContainer(Task):
     ]
 
     def init_task(self) -> None:
+        # Spawn boundary
+        self.spawn_boundary = SpawnBoundary([Shape("spawn_boundary")])
+
         # Object placement point
         self.obj_place = Dummy("object_place")
 
@@ -53,9 +57,40 @@ class CubeContainer(Task):
     def init_episode(self, index: int) -> List[str]:
         """
             Variations:
+
+            WRT object (all possible roations):
             - 0: left container
             - 1: right container
+
+            WRT robot (up to ± 60° rotations):
+            - 2: left container
+            - 3: right container
         """
+        # We use this when we need to swap the target (i.e: if object is spawned with more than 90° rot, 
+        # the L/R boxes will be inverted wrt robot reference frame- )
+        inverted = False
+        # TODO: Use inverted also to change the grasping behaviour -> flip the grasping, approaching, placing 
+        # dummy along downords z by 180 degrees so the picking and placing will be natural
+        # TODO: reduce angles a little bit, maybe 45 degrees
+        # TODO: add 2 variations: nearest and furtest wrt robot, choose the default R box as furthest but check inversion
+        if index in (0, 1):
+            # Sample scene rotations
+            min_rot = (0, 0, -numpy.pi)
+            max_rot = (0, 0, +numpy.pi)
+        else:
+        #elif index in (2, 3):
+            # We need 2 spaces: ±60 and (+120, +240)
+            if numpy.random.random() > 0.5:     # ± 60
+                min_rot = (0, 0, -numpy.pi/3)
+                max_rot = (0, 0, numpy.pi/3)
+            else:                               # 120->240
+                inverted = True # The boxes will be opposite wrt the robot pov
+                min_rot = (0, 0, numpy.pi * (2.0/3.0))
+                max_rot = (0, 0, numpy.pi * (4.0/3.0))
+
+        self.spawn_boundary.clear()
+        self.spawn_boundary.sample(self.obj_place, min_rotation=min_rot, max_rotation=max_rot)
+
         # Load random object
         object_index = numpy.random.randint(len(self.OBJECT_NAMES))
         obj_name = self.OBJECT_NAMES[object_index]
@@ -82,28 +117,49 @@ class CubeContainer(Task):
         self.way3.set_pose(self.approach_dummy.get_pose())  # To rotate the waypoint3 pose similar to graping pose
 
         # Choose appropriate variation
-        # set textual position, refrence dummy, sensor
+        # set textual position, refrence dummy, sensor and generate appropriate task instuction
         if index == 0:
             position = "left"
             reference = Dummy("placeL")
             self.success_sensor = self.sensorL
-        else:
+            instruction = f"Pick up the {obj_name} and place it on the container on its {position} side"
+        elif index == 1:
             position = "right"
             reference = Dummy("placeR")
             self.success_sensor = self.sensorR
+            instruction = f"Pick up the {obj_name} and place it on the container on its {position} side"
+        elif index == 2:
+            # Left wrt robot, but check if inverted spawn position
+            position = "left"
+            if inverted:
+                reference = Dummy("placeR")
+                self.success_sensor = self.sensorR
+            else:
+                reference = Dummy("placeL")
+                self.success_sensor = self.sensorL
+            instruction = f"Pick up the {obj_name} and place it on the container on the {position} side with respect to the robot"
+        else:    # index == 3
+            # Right wrt robot, but check if inverted spawn position
+            position = "right"
+            if inverted:
+                reference = Dummy("placeL")
+                self.success_sensor = self.sensorL
+            else:
+                reference = Dummy("placeR")
+                self.success_sensor = self.sensorR
+            instruction = f"Pick up the {obj_name} and place it on the container on the {position} side with respect to the robot"
 
         # Set waypoint3 to appropriate position (keeping grasping orientation)
         self.way3.set_position(reference.get_position())
         # Register success condition
         success_condition = DetectedCondition(self.object, self.success_sensor)
         self.register_success_conditions([success_condition])
-        # Generate appropriate task instuction
-        instruction = f"Pick up the {obj_name} and place it on the container on its {position} side"
+        
         print(instruction)
         return [instruction]
 
     def variation_count(self) -> int:
-        return 2
+        return 4
 
     def cleanup(self) -> None:
         # Remove added object
@@ -111,7 +167,8 @@ class CubeContainer(Task):
             self.object.remove()
     
     def base_rotation_bounds(self) -> Tuple[Tuple[float, float, float], Tuple[float, float, float]]:
-        return (0.0, 0.0, -numpy.pi/6), (0.0, 0.0, numpy.pi/6)
+        #return (0.0, 0.0, -numpy.pi/6), (0.0, 0.0, numpy.pi/6)
+        return (0, 0, 0), (0, 0, 0)
     
     def _start(self, waypoint: Waypoint):
         assert self.approach_dummy is not None
@@ -121,4 +178,4 @@ class CubeContainer(Task):
         self.way0.set_pose(self.approach_dummy.get_pose())
         self.way1.set_pose(self.grasp_dummy.get_pose())
         self.way2.set_pose(self.approach_dummy.get_pose())
-        pass
+        

affe13e47b9bd605da6536b8befbf476049681a1
SimonReese <59612841+SimonReese@users.noreply.github.com>
2026-04-08 11:44:51 +0200

    Update task with proximity sensors and success conditions

--- rlbench/tasks/cube_container.py
+++ rlbench/tasks/cube_container.py
@@ -3,6 +3,7 @@ import time
 from typing import List, Tuple
 import numpy
 from pyrep.objects.object import Object
+from rlbench.backend.conditions import DetectedCondition
 from rlbench.backend.task import Task
 
 from pyrep.objects import Dummy, ProximitySensor, Shape
@@ -16,8 +17,7 @@ class CubeContainer(Task):
         "joystick controller",
         "mug",
         "pc mouse",
-        "tv remote",
-        #"cube"
+        "tv remote"
     ]
 
     OBJECT_PATHS = [
@@ -25,16 +25,12 @@ class CubeContainer(Task):
         "RLBench/rlbench/assets/controller/controller.ttm",
         "RLBench/rlbench/assets/mug/mug.ttm",
         "RLBench/rlbench/assets/pcmouse/pcmouse.ttm",
-        "RLBench/rlbench/assets/tvremote/tvremote.ttm",
-        #"CUBE_NO_PATH"
+        "RLBench/rlbench/assets/tvremote/tvremote.ttm"
     ]
 
     def init_task(self) -> None:
         # Object placement point
         self.obj_place = Dummy("object_place")
-
-        # Cube object
-        self.cube = Shape("cube")
 
         # Waypoints
         self.way0 = Dummy("waypoint0") # approach waypoint
@@ -42,13 +38,17 @@ class CubeContainer(Task):
         self.way2 = Dummy("waypoint2") # depart waypoint
         self.way3 = Dummy("waypoint3") # release waypoint
 
+        # Proximity sensors
+        self.sensorL = ProximitySensor("sensorL")
+        self.sensorR = ProximitySensor("sensorR")
+
         # Call function after generic approach
         self.register_waypoint_ability_start(0, self._start)
         
         self.object = None
         self.approach_dummy = None
         self.grasp_dummy = None
-        pass
+        self.success_sensor = None
 
     def init_episode(self, index: int) -> List[str]:
         """
@@ -56,21 +56,19 @@ class CubeContainer(Task):
             - 0: left container
             - 1: right container
         """
-        # Load object
+        # Load random object
         object_index = numpy.random.randint(len(self.OBJECT_NAMES))
         obj_name = self.OBJECT_NAMES[object_index]
         obj_path = self.OBJECT_PATHS[object_index]
         print(f"Trying to load {obj_name}")
-        
         model_handle = simLoadModel(obj_path) # spawn object
         self.object = Shape(model_handle)
         self.object.set_parent(self.obj_place)
 
-
         # Set object position
         self.object.set_pose(self.obj_place.get_pose())
 
-        # Register object
+        # Register object as graspable
         self.register_graspable_objects([self.object])
 
         # Get key position
@@ -82,17 +80,23 @@ class CubeContainer(Task):
         self.way1.set_pose(self.grasp_dummy.get_pose())
         self.way2.set_pose(self.approach_dummy.get_pose())
         self.way3.set_pose(self.approach_dummy.get_pose())  # To rotate the waypoint3 pose similar to graping pose
-    
 
+        # Choose appropriate variation
+        # set textual position, refrence dummy, sensor
         if index == 0:
             position = "left"
             reference = Dummy("placeL")
+            self.success_sensor = self.sensorL
         else:
             position = "right"
             reference = Dummy("placeR")
+            self.success_sensor = self.sensorR
 
         # Set waypoint3 to appropriate position (keeping grasping orientation)
         self.way3.set_position(reference.get_position())
+        # Register success condition
+        success_condition = DetectedCondition(self.object, self.success_sensor)
+        self.register_success_conditions([success_condition])
         # Generate appropriate task instuction
         instruction = f"Pick up the {obj_name} and place it on the container on its {position} side"
         print(instruction)
@@ -101,16 +105,10 @@ class CubeContainer(Task):
     def variation_count(self) -> int:
         return 2
 
-    def step(self) -> None:
-        # Called during each sim step. Remove this if not using.
-        pass
-
     def cleanup(self) -> None:
-        # Restore objects
-        if self.object and self.object.get_name() != "cube" and self.object is not None and self.object.exists(self.object.get_name()):
+        # Remove added object
+        if self.object is not None and self.object.still_exists():
             self.object.remove()
-        #self.cube.set_pose(self.obj_place.get_pose())
-        pass
     
     def base_rotation_bounds(self) -> Tuple[Tuple[float, float, float], Tuple[float, float, float]]:
         return (0.0, 0.0, -numpy.pi/6), (0.0, 0.0, numpy.pi/6)
@@ -118,11 +116,9 @@ class CubeContainer(Task):
     def _start(self, waypoint: Waypoint):
         assert self.approach_dummy is not None
         assert self.grasp_dummy is not None
-        self.way0.set_pose(self.approach_dummy.get_pose())
         # Set all waypoints to exact positions after falls of objects
-        print(f"Setting grasping pose: {self.way1.get_pose()[:3]}", end="")
-        #waypoint.get_waypoint_object().set_pose(self.grasp_dummy.get_pose())
+        #waypoint.get_waypoint_object().set_pose(self.grasp_dummy.get_pose()) - not clear if this is has the same result
+        self.way0.set_pose(self.approach_dummy.get_pose())
         self.way1.set_pose(self.grasp_dummy.get_pose())
-        print(f" -> {self.way1.get_pose()[:3]} - [{self.way1} vs {waypoint.get_waypoint_object()}]")
         self.way2.set_pose(self.approach_dummy.get_pose())
         pass
